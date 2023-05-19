d1c4731406efdbf8a0c9ed8d4dbd34822dfdced5
Patryk Szwiec <patryk.szwiec@gmail.com>
2023-05-19 21:11:05 +0200

    Add stopwatch component to create stopwatch app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Container from './components/Container/Container';
-import Button from './components/Button/Button';
+
 import Stopwatch from './components/Stopwatch/Stopwatch';
 
 const App = () => {
   return (
     <Container>
       <Stopwatch/>
-      <Button>START</Button>
-      <Button>STOP</Button>
-      <Button>RESET</Button>
     </Container>
   );
 };
--- src/components/Stopwatch/Stopwatch.js
+++ src/components/Stopwatch/Stopwatch.js
@@ -1,8 +1,74 @@
 import styles from './Stopwatch.module.scss';
+import React, { useState, useEffect } from "react";
+import Button from '../Button/Button';
 
-const Stopwatch = props => {
+const Stopwatch = () => {
+  // state to store time
+  const [time, setTime] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+
+  useEffect(() => {
+    let interval = null;
+
+    // If the stopwatch is active and not paused, start the interval to update the time
+    if (isActive && isPaused === false) {
+      interval = setInterval(() => {
+        // Increment the time by 1 millisecond
+        setTime((time) => time + 1);
+      }, 1);
+    } else {
+      // If the stopwatch is not active or is paused, clear the interval
+      clearInterval(interval);
+    }
+    return () => {
+      // Clean up the interval when the component is unmounted or when the dependencies change
+      clearInterval(interval);
+    };
+  }, [isActive, isPaused]);
+
+  // Hours calculation
+  const hours = Math.floor(time / 360000);
+
+  // Minutes calculation
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  // Seconds calculation
+  const seconds = Math.floor((time % 6000) / 100);
+
+  // Milliseconds calculation
+  const milliseconds = time % 100 ;
+
+  // Method to start timer
+  const startTimer = () => {
+    setIsActive(true);
+    setIsPaused(false);
+  };
+
+  // Method to stop timer
+  const stopTimer = () => {
+    setIsPaused(true);
+  };
+
+  // Method to reset timer
+  const resetTimer = () => {
+    setIsActive(false);
+    setTime(0);
+  };
+
   return (
-    <div className={styles.stopwatch}></div>
+    <div>
+      <p className={styles.stopwatch}>
+        {hours.toString().padStart(2, "0")}:
+        {minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}.
+        {milliseconds.toString().padStart(2, "0")}
+      </p>
+      {/* <Button onClick={startTimer}>START</Button> */}
+      <Button onClick={startTimer}>START</Button>
+      <Button onClick={stopTimer}>STOP</Button>
+      <Button onClick={resetTimer}>RESET</Button>
+    </div>
   );
 };
 
